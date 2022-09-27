9b0d6b70fd9ef643e127a40f87856fc0e44a28cf
Ryan Obbink <ryanobbink@gmail.com>
2022-09-26 18:02:34 -0600

    Change Project to Keno.Core and Add Keno.Admin. Add json file for PayTable

--- MultiCard4Keno/Interfaces/ICard.cs
+++ MultiCard4Keno/Interfaces/ICard.cs
@@ -4,18 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MultiCard4Keno.Enums;
+using Keno.Core.Enums;
 
-namespace MultiCard4Keno.Interfaces
+namespace Keno.Core.Interfaces
 {
     public interface ICard
     {
         public const int _spots = 80;
+
+
         public bool CanDraw { get; }
 
         public CardName Name { get; }
+
         public List<int> Marked { get; set; }
-        public List<int> Drawn { get; }
+
+        public List<int> Drawn { get; set; }
+
+        public bool Win { get; }
 
         public bool Mark(int n);
         public void QuickPick();
